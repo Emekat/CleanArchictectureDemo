46c76756b8542f79c8fb9f728735319fb907d5ba
emekat <emekatorti@gmail.com>
2023-02-20 01:37:08 -0500

    Implementation of Course service and repo

---
+++ CleanArch/CleanArch.Infra.Data/Repositories/CourseRepository.cs
@@ -0,0 +1,24 @@
+﻿using CleanArch.Domain.Models;
+using CleanArch.Domain.Models.Interfaces;
+using CleanArch.Infra.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArch.Infra.Data.Repositories
+{
+    public class CourseRepository : ICourseRepository
+    {
+        private readonly UniversityDbContext _ctx;
+
+        public CourseRepository(UniversityDbContext ctx)
+        {
+           _ctx = ctx;
+        }
+
+        public IEnumerable<Course> GetCourses()
+        {
+            return _ctx.Courses;
+        }
+    }
+}
